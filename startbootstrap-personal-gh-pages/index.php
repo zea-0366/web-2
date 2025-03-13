52b82ddad6ca72efe08b279f91daa87548b1d0be
Fariza Zea <farizazea9c@gmail.com>
2025-03-13 12:28:12 +0700

    upload

---
+++ startbootstrap-personal-gh-pages/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once("function/CallPage.php");
+callPage("header");
+callPage("navbar"); 
+if (isset($_GET['page'])) {
+  callPage($_GET['page']);
+}else{
+  callPage("home");
+} {
+
+}
+callPage("footer");
+
+
+
+
+?>
